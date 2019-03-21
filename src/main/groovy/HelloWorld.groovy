bdfe24603fca33fa87433be5ba37b6c46851aec1
tadashi endo <tadaedo@gmail.com>
2019-03-21 18:04:51 +0900

    jsf sample

---
+++ src/main/groovy/HelloWorld.groovy
@@ -0,0 +1,10 @@
+@javax.faces.bean.ManagedBean
+public class HelloWorld {
+
+  def firstName = "John"
+  def lastName = "Doe"
+
+  def showGreeting() {
+    return "Hello " + firstName + " " + lastName + "!";
+  }
+}
